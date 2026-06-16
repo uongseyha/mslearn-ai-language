f663bbcf43c0108183af42b80f406fc1de4809bc
Seyha Uong <uong.seyha@gmail.com>
2026-06-16 09:37:42 -0400

    Imp voice live

--- Labfiles/06-voice-live/Python/chat-client/chat-client.py
+++ Labfiles/06-voice-live/Python/chat-client/chat-client.py
@@ -1,3 +1,4 @@
+from multiprocessing.dummy import connection
 import os
 import asyncio
 import base64
@@ -6,6 +7,18 @@ from dotenv import load_dotenv
 import pyaudio
 
 # import namespaces
+from azure.identity.aio import AzureCliCredential
+from azure.ai.voicelive.aio import connect
+from azure.ai.voicelive.models import (
+    InputAudioFormat,
+    Modality,
+    OutputAudioFormat,
+    RequestSession,
+    ServerEventType,
+    AudioNoiseReduction,
+    AudioEchoCancellation,
+    AzureSemanticVadMultilingual
+) 
 
 
 
@@ -76,18 +89,32 @@ class VoiceAssistant:
         # Add your code in this try block!
         try:
             # STEP 1: Connect Azure VoiceLive to the agent
+            async with connect(
+                endpoint=self.endpoint,
+                credential=self.credential,
+                api_version="2026-01-01-preview",
+                agent_config=self.agent_config
+            ) as connection:
+                self.connection = connection
 
                 
                 # STEP 2: Initialize audio processor
+                self.audio_processor = AudioProcessor(connection)
                 
                 
                 # STEP 3: Configure the session
+                await self.setup_session()
                 
                 
                 # STEP 4: Start audio systems
+                self.audio_processor.start_playback()
+                        
+                print("\n✅ Ready! Start speaking...")
+                print("Press Ctrl+C to exit\n")
                 
                 
                 # STEP 5: Process events
+                await self.process_events()
                 
 
         
